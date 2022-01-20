dc4c0744bc296a7596ed0dde333558236ea67fc4
adamdavies1 <adam.davies@evoco.co.uk>
2022-01-20 10:40:44 +0000

    Implementing new lighthouse-ci

---
+++ lighthouserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        "http://localhost:3000/app",
+        "http://localhost:3000/app/new",
+        "http://localhost:3000/app/choose-existing",
+        "http://localhost:3000/app/designer/test-form-a",
+        "http://localhost:3009/components/all-components",
+      ],
+      startServerCommand: "",
+    },
+    assert: {
+      preset: "lighthouse:recommended",
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
